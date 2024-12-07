cfac827ee0d73664678949ee49c8da2c9d4871a8
Aayush <abbharadwaj@wisc.edu>
2024-12-07 16:34:07 -0600

    delete and insert

--- delete.C
+++ delete.C
@@ -16,11 +16,44 @@ const Status QU_Delete(const string & relation,
 		       const Datatype type, 
 		       const char *attrValue)
 {
-// part 6
-return OK;
+    Status status;
+    HeapFileScan fileScan(relation, status);
+    if (status != OK) {
+        return status;
+    }
 
+    AttrDesc attrDesc;
+    status = attrCat->getInfo(relation, attrName, attrDesc);
+    if (status != OK) {
+        return status;
+    }
 
+    status = fileScan.startScan(attrDesc.attrOffset,
+                                attrDesc.attrLen,
+                                type,
+                                attrValue,
+                                op);
+    if (status != OK) {
+        return status;
+    }
 
+    RID rid;
+    Record rec;
+    while (fileScan.scanNext(rid) == OK) {
+        status = fileScan.getRecord(rec);
+        if (status != OK) {
+            return status;
+        }
+
+        status = fileScan.deleteRecord();
+        if (status != OK) {
+            return status;
+        }
+    }
+
+    fileScan.endScan();
+
+    return OK;
 }
 
 
--- insert.C
+++ insert.C
@@ -1,5 +1,6 @@
 #include "catalog.h"
 #include "query.h"
+#include "heapfile.h"
 
 
 /*
@@ -14,8 +15,78 @@ const Status QU_Insert(const string & relation,
 	const int attrCnt, 
 	const attrInfo attrList[])
 {
-// part 6
-return OK;
+    RelDesc relDesc;
+    Status status = relCat->getInfo(relation, relDesc);
+    if (status != OK) {
+        return status; // Relation not found
+    }
 
+    // Retrieve attributes of the relation
+    AttrDesc *attrDesc;
+    int relAttrCnt;
+    status = attrCat->getRelInfo(relation, relAttrCnt, attrDesc);
+    if (status != OK) {
+        return status; // Error retrieving attributes
+    }
+
+    // Validate attribute count
+    if (attrCnt != relAttrCnt) {
+        return ATTRNOTFOUND;
+    }
+
+    // Allocate memory for the record
+    int recordLength = 0;
+    for (int i = 0; i < relAttrCnt; ++i) {
+        recordLength += attrDesc[i].attrLen;
+    }
+    char *recordData = new char[recordLength];
+    memset(recordData, 0, recordLength); // Initialize memory
+
+    // Copy attribute values into the record
+    for (int i = 0; i < attrCnt; ++i) {
+        const attrInfo &attr = attrList[i];
+        bool found = false;
+
+        // Find the attribute in the relation schema
+        for (int j = 0; j < relAttrCnt; ++j) {
+            if (attrDesc[j].attrName == attr.attrName) {
+                // Validate attribute type
+                if (attrDesc[j].attrType != attr.attrType) {
+                    delete[] recordData;
+                    return ATTRTYPEMISMATCH;
+                }
+
+                // Copy the attribute value into the record
+                memcpy(recordData + attrDesc[j].attrOffset, attr.attrValue, attrDesc[j].attrLen);
+                found = true;
+                break;
+            }
+        }
+
+        if (!found) {
+            delete[] recordData;
+            return ATTRNOTFOUND; // Attribute not in schema
+        }
+    }
+
+    // Open heap file for the relation
+    Status heapStatus;
+    InsertFileScan heapFile(relation, heapStatus);
+    if (heapStatus != OK) {
+        delete[] recordData;
+        return heapStatus; // Error opening heap file
+    }
+
+    // Insert the record into the heap file
+    RID rid;
+    Record newRecord = {recordData, recordLength};
+    heapStatus = heapFile.insertRecord(newRecord, rid);
+    delete[] recordData; // Free allocated memory
+
+    if (heapStatus != OK) {
+        return heapStatus; // Error inserting record
+    }
+
+    return OK;
 }
 
